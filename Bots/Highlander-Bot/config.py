3a5f2e0862818df8aa7b08d2832736c68c822fe9
billy <billy@billy.com>
2020-09-05 22:06:51 -0400

    hpa

--- Bots/Highlander-Bot/config.py
+++ Bots/Highlander-Bot/config.py
@@ -1,4 +1,3 @@
 username = 'HighlanderBot'
-password = '1ash2ash'
 client_secret = 'QNnBQf-oe0IF-dXg94NMONz3uEk'
 client_id = 'TdxsI90kK-rDkQ'
--- Bots/Urban-Dictionary-Bot/config.py
+++ Bots/Urban-Dictionary-Bot/config.py
@@ -1,4 +1,3 @@
 username = 'UrbanDictBot'
-password = '1ash2ash'
 client_secret = 'f3vVgofP3Wwoabh9bUWw40yUfpo'
 client_id = 'lJ92Yc1XvVDMvQ'
