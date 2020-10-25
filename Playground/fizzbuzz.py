e62a6575cc3bd0718b2383bfe47e8aa55eda3519
billy <billy@billy.com>
2020-10-25 17:45:16 -0400

    🍕 fizzbuzz oneliner printing

--- Playground/fizzbuzz.py
+++ Playground/fizzbuzz.py
@@ -1,3 +1,3 @@
-fizzbuzz = lambda n: '\n'.join([f'{i}\t{"Fizz" * (i % 3==0)}{"Buzz" * (i % 5==0)}{"Baxx" * (i % 7==0)}' for i in range(1, n+1)])
+fizzbuzz = lambda n: print('\n'.join([f'{i}\t{"Fizz" * (i % 3==0)}{"Buzz" * (i % 5==0)}{"Baxx" * (i % 7==0)}' for i in range(1, n+1)]))
 
-print(fizzbuzz(500))
+fizzbuzz(500)
