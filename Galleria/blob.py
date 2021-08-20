974801e6ade66b231196051580413159e136e8dd
ddmin <ddmin216@outlook.com>
2021-08-19 20:00:21 -0400

    🐘 img

--- Galleria/blob.py
+++ Galleria/blob.py
@@ -1,8 +1,14 @@
-import wget
 import requests
 from bs4 import BeautifulSoup
 
 url = input('URL: ')
+
+top_url = input('Top Level Domain URL (leave blank for none): ')
+if top_url:
+    TOP_LEVEL = True
+else:
+    TOP_LEVEL = False
+
 SEARCH_FOR = input('Searching for: ')
 FILE_FORMAT = input('File Format: ')
 COUNT_FROM = int(input('Count From: '))
@@ -20,8 +26,22 @@ for result in links:
 
 for n, i in enumerate(dl):
     print()
-    print(i['href'])
-    if 'href' in i.attrs:
-        LINK = i['href']
-        print(f"Downloading {LINK}")
-        wget.download(LINK, out=f'{COUNT_FROM + n}.{FILE_FORMAT}')
+    try:
+        if 'href' in i.attrs:
+            LINK = i['href']
+
+            if TOP_LEVEL:
+                LINK = top_url + LINK
+
+            print(f"Downloading {LINK}")
+            # wget.download(LINK, out=f'{COUNT_FROM + n}.{FILE_FORMAT}')
+
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+            response = requests.get(LINK, headers=headers)
+
+            with open(f'{COUNT_FROM + n}.{FILE_FORMAT}', 'wb') as img:
+                img.write(response.content)
+    except:
+        print("Error. Skipping.\n")
+        continue
