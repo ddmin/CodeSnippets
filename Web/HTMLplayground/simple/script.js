6b46f4899833f401bd4ba6f0ede8068fae5db1e7
ddmin <danieldmin216@gmail.com>
2021-10-24 00:21:56 -0400

    🐯 not mine

--- Web/HTMLplayground/simple/script.js
+++
@@ -1,9 +0,0 @@
-function darkMode() {
-    var theme = "dark-mode";
-    document.getElementById("body").className = theme;
-}
-
-function lightMode() {
-    var theme = "light-mode";
-    document.getElementById("body").className = theme;
-}
