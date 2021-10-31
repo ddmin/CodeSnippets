dbb8ac673905a54ef9abe133cdc88b033fbe3a8f
ddmin <danieldmin216@gmail.com>
2021-10-30 20:47:08 -0400

    🐦 twitter script

--- RS/scraper/src/main.rs
+++ RS/scraper/src/main.rs
@@ -1,35 +1,11 @@
-use roux;
-use roux::util::{FeedOption, TimePeriod};
-use std::fs;
+use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // const USER_AGENT: &str = "linux:rust (by /u/alterproncount)";
-
-    // let contents = fs::read_to_string("secrets.conf").expect("Could not read file.");
-    // let contents = contents.split_whitespace();
-
-    // let contents: Vec<_> = contents.collect();
-    // let username = contents[0];
-    // let password = contents[1];
-    // let client_secret = contents[2];
-    // let client_id = contents[3];
-
-    // let client = roux::Reddit::new(USER_AGENT, client_id, client_secret)
-    //     .username(username)
-    //     .password(password)
-    //     .login()
-    //     .await;
-
-    // let me = client.unwrap();
-    // let upvoted = me.upvoted().await.unwrap().data;
-
-    let subreddit = roux::Subreddit::new("astolfo");
-
-    // Gets top 10 posts from this month
-    let options = FeedOption::new().period(TimePeriod::ThisMonth);
-    let top = subreddit.top(25, Some(options)).await;
-    println!("{:?}", top);
-
+    let query = "apple";
+    let link = format!("https://en.wikipedia.org/wiki/{}", query);
+    println!("{}", link);
+    let resp = reqwest::get(link).await?.text().await?;
+    println!("{:#?}", resp);
     Ok(())
 }
