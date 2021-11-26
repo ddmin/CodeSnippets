218542444963559827d5849aa5cf961c2532ec61
ddmin <danieldmin216@gmail.com>
2021-11-25 22:40:14 -0500

    ⚔️ bunshin no jutsu

--- TEST/bunshin-no-jutsu.sh
+++ TEST/bunshin-no-jutsu.sh
@@ -7,14 +7,16 @@ EGRESS="$HOME/.local/hide"
 mkdir -p $EGRESS
 
 # directories to move
-dirs=("Code" "Desktop" "Documents" "Downloads" "Music" "Pictures" "Public" "Templates" "Videos" "VirtualBox")
+dirs=("Code" "Documents" "Downloads" "Music" "Pictures" "Public" "Templates" "Videos" "VirtualBox")
 
 # loop through directories
 for d in ${dirs[@]}; do
 
     if [ "$1" == "reset" ]; then
+        [ -d "$HOME/$d" ] && echo "$HOME/$d already exists." && echo "Try removing \`reset\`." && exit
         mv "$EGRESS/$d" "$HOME/$d"
     else
+        [ -d "$EGRESS/$d" ] && echo "$EGRESS/$d already exists." && echo "Try adding \`reset\`." && exit
         mv "$HOME/$d" "$EGRESS/$d"
     fi
 
