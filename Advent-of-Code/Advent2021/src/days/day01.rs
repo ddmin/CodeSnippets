95c3392c7e2664c1431ed4b26b319e58d4ef327a
ddmin <danieldmin216@gmail.com>
2021-12-01 09:42:11 -0500

    🎄 better day 1 solution
    🤭 base < cmp

--- Advent-of-Code/Advent2021/src/days/day01.rs
+++ Advent-of-Code/Advent2021/src/days/day01.rs
@@ -2,15 +2,15 @@ use crate::*;
 
 pub fn part1(input: &str) {
     let input = lines_to_i32(input);
-    let (mut base_iter, cmp_iter) = (input.iter(), input.iter());
+    let (base_iter, mut cmp_iter) = (input.iter(), input.iter());
 
-    base_iter.next();
+    cmp_iter.next();
 
-    // base: 1 2 3 4 5 ...
-    // cmp : 0 1 2 3 4 ...
+    // base: 0 1 2 3 4 ...
+    // cmp : 1 2 3 4 5 ...
     let solution = base_iter
         .zip(cmp_iter)
-        .map(|(&base, &cmp)| base > cmp)
+        .map(|(&base, &cmp)| base < cmp)
         .filter(|&bool| bool)
         .count();
 
@@ -19,20 +19,15 @@ pub fn part1(input: &str) {
 
 pub fn part2(input: &str) {
     let input = lines_to_i32(input);
-    let mut triplets: Vec<i32> = Vec::new();
-    for i in 0..input.len() - 2 {
-        triplets.push(input[i..i + 3].iter().sum());
-    }
+    let (base_iter, cmp_iter) = (input.iter(), input.iter().skip(3));
 
-    let (mut base_iter, cmp_iter) = (triplets.iter(), triplets.iter());
-
-    base_iter.next();
-
-    // base: 1 2 3 4 5 ...
-    // cmp : 0 1 2 3 4 ...
+    // A
+    // A   B   <-- Equal
+    // A   B   <-- Equal
+    //     B
     let solution = base_iter
         .zip(cmp_iter)
-        .map(|(&base, &cmp)| base > cmp)
+        .map(|(&base, &cmp)| base < cmp)
         .filter(|&bool| bool)
         .count();
 
