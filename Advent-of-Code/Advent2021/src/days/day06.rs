fc2192372fa8a58aa5af2d700beefa792b9afc9e
ddmin <danieldmin216@gmail.com>
2021-12-06 21:26:34 -0500

    🎄 Day 6 - part 1, part 2 is a math question 🐟

--- Advent-of-Code/Advent2021/src/days/day06.rs
+++ Advent-of-Code/Advent2021/src/days/day06.rs
@@ -1,21 +1,56 @@
-#![allow(unused_imports)]
-use crate::*;
-
 const INPUT: &str = include_str!("../../inputs/day06.txt");
 
-#[allow(unused)]
-pub fn part1(input: &str) -> i32 {
-    0
+pub fn part1(input: &str, days: i32) -> i32 {
+    let mut lanternfish = input
+        .split(',')
+        .map(|n| n.parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
+
+    for _ in 0..days {
+        let mut new_fish = Vec::new();
+        lanternfish = lanternfish
+            .iter()
+            .map(|&n| {
+                if n == 0 {
+                    new_fish.push(8);
+                    6
+                } else {
+                    n - 1
+                }
+            })
+            .collect();
+        lanternfish.append(&mut new_fish);
+    }
+    lanternfish.len() as i32
 }
 
-#[allow(unused)]
-pub fn part2(input: &str) -> i32 {
-    0
+pub fn part2(input: &str, days: i32) -> i64 {
+    let mut lanternfish = input
+        .split(',')
+        .map(|n| n.parse::<i32>().unwrap())
+        .collect::<Vec<_>>();
+
+    for _ in 0..days {
+        let mut new_fish = Vec::new();
+        lanternfish = lanternfish
+            .iter()
+            .map(|&n| {
+                if n == 0 {
+                    new_fish.push(8);
+                    6
+                } else {
+                    n - 1
+                }
+            })
+            .collect();
+        lanternfish.append(&mut new_fish);
+    }
+    lanternfish.len() as i64
 }
 
 pub fn run() {
-    println!("Part 1: {}", part1(INPUT));
-    println!("Part 2: {}", part2(INPUT));
+    println!("Part 1: {}", part1(INPUT, 80));
+    println!("Part 2: {}", part2(INPUT, 256));
 }
 
 #[cfg(test)]
@@ -25,11 +60,11 @@ mod tests {
 
     #[test]
     fn example1() {
-        assert_eq!(part1(EXAMPLE), 0);
+        assert_eq!(part1(EXAMPLE, 18), 26);
     }
 
     #[test]
     fn example2() {
-        assert_eq!(part2(EXAMPLE), 0);
+        assert_eq!(part2(EXAMPLE, 256), 26984457539);
     }
 }
--- Advent-of-Code/Advent2021/src/main.rs
+++ Advent-of-Code/Advent2021/src/main.rs
@@ -1,6 +1,6 @@
 use advent_2021::run_days;
 
 fn main() {
-    let days = (1..=5).collect::<Vec<_>>();
+    let days = (1..=6).collect::<Vec<_>>();
     run_days(days);
 }
