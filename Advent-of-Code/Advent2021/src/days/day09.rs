5d6f6d68e22477581b8354ac17bb58bdefb97ec2
ddmin <danieldmin216@gmail.com>
2021-12-09 15:50:26 -0500

    🎄 Day 9 - part 1, part 2 seems hard

--- Advent-of-Code/Advent2021/src/days/day09.rs
+++ Advent-of-Code/Advent2021/src/days/day09.rs
@@ -1,15 +1,64 @@
-#![allow(unused_imports)]
-use crate::*;
-
 const INPUT: &str = include_str!("../../inputs/day09.txt");
 
-#[allow(unused)]
-pub fn part1(input: &str) -> i32 {
-    0
+type HeightMap = Vec<Vec<i32>>;
+
+fn parse_input(input: &str) -> HeightMap {
+    input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_string().parse::<i32>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect()
 }
 
-#[allow(unused)]
+fn neighbors(grid: &[Vec<i32>], x: usize, y: usize) -> Vec<i32> {
+    let width: usize = grid[0].len() - 1;
+    let height: usize = grid.len() - 1;
+
+    let mut neighbors = Vec::new();
+
+    // Adjacent Left Neighbor
+    if x != 0 {
+        neighbors.push(grid[y][x - 1]);
+    }
+
+    // Adjacent Right Neighbor
+    if x < width {
+        neighbors.push(grid[y][x + 1]);
+    }
+
+    // Adjacent Bottom Neighbor
+    if y < height {
+        neighbors.push(grid[y + 1][x]);
+    }
+
+    // Adjacent Top Neighbor
+    if y != 0 {
+        neighbors.push(grid[y - 1][x]);
+    }
+    neighbors
+}
+
+fn is_lowest(n: i32, neighbors: &[i32]) -> bool {
+    neighbors.iter().find(|&&x| n >= x).is_none()
+}
+
+pub fn part1(input: &str) -> i32 {
+    let heightmap = parse_input(input);
+
+    (0..heightmap.len())
+        .map(|y| {
+            (0..heightmap[y].len())
+                .filter(|&x| is_lowest(heightmap[y][x], &neighbors(&heightmap, x, y)))
+                .fold(0, |acc, x| acc + 1 + heightmap[y][x])
+        })
+        .sum()
+}
+
 pub fn part2(input: &str) -> i32 {
+    let _heightmap = parse_input(input);
     0
 }
 
@@ -25,11 +74,11 @@ mod tests {
 
     #[test]
     fn example1() {
-        assert_eq!(part1(EXAMPLE), 0);
+        assert_eq!(part1(EXAMPLE), 15);
     }
 
     #[test]
     fn example2() {
-        assert_eq!(part2(EXAMPLE), 0);
+        assert_eq!(part2(EXAMPLE), 1134);
     }
 }
