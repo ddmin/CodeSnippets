e8cdf742a5a0d01f2358d690676614646ccf6dd2
ddmin <danieldmin216@gmail.com>
2021-12-11 18:34:05 -0500

    👻 Day 10 - part 1

--- Advent-of-Code/Advent2021/src/days/day10.rs
+++ Advent-of-Code/Advent2021/src/days/day10.rs
@@ -1,11 +1,41 @@
-#![allow(unused_imports)]
-use crate::*;
+fn parse_input(input: &str) -> Vec<&str> {
+    input.lines().collect()
+}
+
+fn mirror(c: char) -> char {
+    let mut characters = vec!['(', '[', '{', '<', ')', ']', '}', '>'];
+    let idx = characters.iter().position(|&x| c == x).unwrap();
+    characters.rotate_right(4);
+    characters[idx]
+}
 
 const INPUT: &str = include_str!("../../inputs/day10.txt");
 
-#[allow(unused)]
 pub fn part1(input: &str) -> i32 {
-    0
+    let mut score = 0;
+    for chunk in parse_input(input) {
+        let mut stack = Vec::new();
+        for c in chunk.chars() {
+            match c {
+                '(' | '[' | '{' | '<' => stack.push(c),
+                ')' | ']' | '}' | '>' => {
+                    let top = stack.pop().unwrap();
+                    if mirror(top) != c {
+                        score += match c {
+                            ')' => 3,
+                            ']' => 57,
+                            '}' => 1197,
+                            '>' => 25137,
+                            _ => unreachable!(),
+                        };
+                        break;
+                    }
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+    score
 }
 
 #[allow(unused)]
@@ -25,7 +55,7 @@ mod tests {
 
     #[test]
     fn example1() {
-        assert_eq!(part1(EXAMPLE), 0);
+        assert_eq!(part1(EXAMPLE), 26397);
     }
 
     #[test]
