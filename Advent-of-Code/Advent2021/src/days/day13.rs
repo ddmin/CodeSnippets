46ab91850243f2c079935b2ed2bb50f241090e32
ddmin <danieldmin216@gmail.com>
2021-12-13 20:57:00 -0500

    🎄 Day 13 - fix flipping, better algorithm (2n - x)

--- Advent-of-Code/Advent2021/src/days/day13.rs
+++ Advent-of-Code/Advent2021/src/days/day13.rs
@@ -1,112 +1,71 @@
 use crate::*;
+use std::collections::HashSet;
 
 const INPUT: &str = include_str!("../../inputs/day13.txt");
 
-#[derive(Debug)]
 enum Fold {
     Vertical(usize),
     Horizontal(usize),
 }
 
-#[derive(Clone)]
-enum Mark {
-    Marked,
-    Unmarked,
-}
-
-impl Mark {
-    fn is_marked(&self) -> bool {
-        match self {
-            Mark::Marked => true,
-            Mark::Unmarked => false,
-        }
-    }
-}
-
-impl fmt::Debug for Mark {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Mark::Marked => write!(f, "#"),
-            Mark::Unmarked => write!(f, "."),
-        }
-    }
-}
-
 struct Paper {
-    paper: Vec<Vec<Mark>>,
+    marked: HashSet<Coordinate>,
+    width: usize,
+    height: usize,
 }
 
 type Coordinate = (usize, usize);
 
 impl Paper {
-    fn new(dots: Vec<Coordinate>) -> Paper {
+    fn new(marked: HashSet<Coordinate>) -> Paper {
         let (mut width, mut height) = (0, 0);
-        dots.iter().for_each(|(x, y)| {
+        marked.iter().for_each(|(x, y)| {
             width = width.max(*x);
             height = height.max(*y);
         });
 
-        let mut paper = vec![vec![Mark::Unmarked; width + 1]; height + 1];
-        dots.iter().for_each(|(x, y)| paper[*y][*x] = Mark::Marked);
-
-        Paper { paper }
-    }
-
-    fn fold(&mut self, folds: &[Fold]) {
-        for fold in folds {
-            match fold {
-                Fold::Horizontal(crease) => {
-                    for col in 0..self.paper.len() {
-                        for row in 0..self.paper[col].len() {
-                            // if self.paper[col][self.paper[col].len() - row - 1].is_marked() {
-                            //     self.paper[col][row] = Mark::Marked;
-                            // }
-                            if self.paper[col][self.paper[col].len() - row - 1].is_marked() {
-                                self.paper[col][row] = Mark::Marked;
-                            }
-                        }
-                    }
-                    self.paper = self
-                        .paper
-                        .iter()
-                        .map(|row| {
-                            row.iter()
-                                .enumerate()
-                                .filter(|(idx, _)| idx > crease)
-                                .map(|(_, mark)| mark.clone())
-                                .collect::<Vec<_>>()
-                        })
-                        .collect::<Vec<_>>()
-                }
-                Fold::Vertical(crease) => {
-                    for col in 0..=*crease {
-                        for row in 0..self.paper[col].len() {
-                            if self.paper[self.paper.len() - col - 1][row].is_marked() {
-                                self.paper[col][row] = Mark::Marked;
-                            }
-                        }
-                    }
-                    let (top, _bottom) = self.paper.split_at(*crease);
-                    self.paper = top.to_vec();
-                }
-            }
+        Paper {
+            marked,
+            width,
+            height,
         }
     }
 
-    fn count_marked(&self) -> i32 {
-        self.paper
-            .iter()
-            .map(|row| row.iter().filter(|mark| mark.is_marked()))
-            .flatten()
-            .count() as i32
+    fn fold(&mut self, fold: &Fold) {
+        self.marked = match fold {
+            Fold::Horizontal(crease) => {
+                self.width = *crease;
+                self.marked
+                    .iter()
+                    .map(|&coordinate| match coordinate {
+                        (x, y) if x < *crease => (x, y),
+                        (x, y) => (crease * 2 - x, y),
+                    })
+                    .collect::<HashSet<_>>()
+            }
+            Fold::Vertical(crease) => {
+                self.height = *crease;
+                self.marked
+                    .iter()
+                    .map(|&coordinate| match coordinate {
+                        (x, y) if y < *crease => (x, y),
+                        (x, y) => (x, crease * 2 - y),
+                    })
+                    .collect::<HashSet<_>>()
+            }
+        }
     }
 }
 
 impl fmt::Debug for Paper {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for row in self.paper.iter() {
-            for mark in row.iter() {
-                write!(f, "{:?}", mark)?;
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                if self.marked.contains(&(x, y)) {
+                    write!(f, "█")?;
+                } else {
+                    write!(f, ".")?;
+                }
             }
             writeln!(f)?;
         }
@@ -120,7 +79,7 @@ fn parse_input(input: &str) -> (Paper, Vec<Fold>) {
     let dots = bifurcate(input.next().unwrap(), ",")
         .into_iter()
         .map(|(a, b)| (a.parse::<usize>().unwrap(), b.parse::<usize>().unwrap()))
-        .collect::<Vec<_>>();
+        .collect::<HashSet<_>>();
 
     let folds = bifurcate(input.next().unwrap(), "=")
         .into_iter()
@@ -136,17 +95,13 @@ fn parse_input(input: &str) -> (Paper, Vec<Fold>) {
 
 pub fn part1(input: &str) -> i32 {
     let (mut paper, folds) = parse_input(input);
-
-    // Part 1: only fold once
-    let (first, _) = folds.split_at(1);
-
-    paper.fold(first);
-    paper.count_marked()
+    paper.fold(&folds[0]);
+    paper.marked.len() as i32
 }
 
 pub fn part2(input: &str) {
     let (mut paper, folds) = parse_input(input);
-    paper.fold(&folds);
+    folds.iter().for_each(|fold| paper.fold(fold));
     println!("{:?}", paper);
 }
 
