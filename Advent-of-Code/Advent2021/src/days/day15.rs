bb51ca1301190ba32cb1ef376d4fcc25ebd95427
ddmin <danieldmin216@gmail.com>
2021-12-15 12:50:09 -0500

    🎄 Day 15 - really really naive recursive solution

--- Advent-of-Code/Advent2021/src/days/day15.rs
+++ Advent-of-Code/Advent2021/src/days/day15.rs
@@ -1,11 +1,48 @@
-#![allow(unused_imports)]
-use crate::*;
-
 const INPUT: &str = include_str!("../../inputs/day15.txt");
 
-#[allow(unused)]
+type Cave = Vec<Vec<usize>>;
+type Coordinate = (usize, usize);
+
+fn parse_input(input: &str) -> Cave {
+    input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).unwrap() as usize)
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>()
+}
+
+fn find_neighbors((x, y): Coordinate, cave: &Cave) -> Vec<Coordinate> {
+    [
+        (x < cave[0].len() - 1, (x + 1, y)),
+        (y < cave.len() - 1, (x, y + 1)),
+    ]
+    .into_iter()
+    .filter(|(condition, _)| *condition)
+    .map(|(_, coordinate)| coordinate)
+    .collect::<Vec<_>>()
+}
+
+fn find_lowest_risk((x, y): Coordinate, risk_level: usize, cave: &Cave) -> usize {
+    if (x, y) == (cave[0].len() - 1, cave.len() - 1) {
+        return risk_level;
+    }
+
+    let mut risk_levels = find_neighbors((x, y), &cave)
+        .into_iter()
+        .map(|(nx, ny)| find_lowest_risk((nx, ny), risk_level + cave[ny][nx], &cave))
+        .collect::<Vec<_>>();
+
+    risk_levels.sort();
+
+    *risk_levels.get(0).unwrap()
+}
+
 pub fn part1(input: &str) -> i32 {
-    0
+    let cave = parse_input(input);
+    find_lowest_risk((0, 0), 0, &cave) as i32
 }
 
 #[allow(unused)]
@@ -25,7 +62,7 @@ mod tests {
 
     #[test]
     fn example1() {
-        assert_eq!(part1(EXAMPLE), 0);
+        assert_eq!(part1(EXAMPLE), 40);
     }
 
     #[test]
